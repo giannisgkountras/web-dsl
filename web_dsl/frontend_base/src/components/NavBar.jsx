e1fc11357702ed95496a065bb63e7cd5457b64ae
giannisgkountras <gkountrasioannis@gmail.com>
2025-03-13 19:58:01 +0200

    Made rows and colums responsive + support custom width & height

--- web_dsl/frontend_base/src/components/NavBar.jsx
+++ web_dsl/frontend_base/src/components/NavBar.jsx
@@ -2,43 +2,43 @@ import { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 
 const NavBar = ({ screens }) => {
-  const [activeUrl, setActiveUrl] = useState("");
-  const location = useLocation();
+    const [activeUrl, setActiveUrl] = useState("");
+    const location = useLocation();
 
-  useEffect(() => {
-    // Normalize URLs by removing trailing slashes
-    const currentPath = location.pathname.replace(/\/$/, "");
-    const matchingScreen = screens.find(
-      (screen) => currentPath === screen.url.replace(/\/$/, ""),
+    useEffect(() => {
+        // Normalize URLs by removing trailing slashes
+        const currentPath = location.pathname.replace(/\/$/, "");
+        const matchingScreen = screens.find(
+            (screen) => currentPath === screen.url.replace(/\/$/, "")
+        );
+
+        if (matchingScreen) {
+            setActiveUrl(matchingScreen.url);
+        }
+    }, [location.pathname, screens]);
+
+    return (
+        <div className="w-full h-[56px] top-0 flex justify-center">
+            <div className="w-1/2 h-full flex items-center justify-evenly px-4 gap-4">
+                {screens.map((screen) => {
+                    const isActive = activeUrl === screen.url;
+                    return (
+                        <Link
+                            key={screen.url} // Use URL as unique key
+                            to={screen.url}
+                            className={`text-lg w-full text-center font-bold px-4 py-2 transition-all ${
+                                isActive
+                                    ? "border-b-2 border-white text-white"
+                                    : "text-gray-600 hover:border-b-2 "
+                            }`}
+                        >
+                            {screen.title}
+                        </Link>
+                    );
+                })}
+            </div>
+        </div>
     );
-
-    if (matchingScreen) {
-      setActiveUrl(matchingScreen.url);
-    }
-  }, [location.pathname, screens]);
-
-  return (
-    <div className="w-full h-14 top-0 flex justify-center">
-      <div className="w-1/2 h-full flex items-center justify-evenly px-4 gap-4">
-        {screens.map((screen) => {
-          const isActive = activeUrl === screen.url;
-          return (
-            <Link
-              key={screen.url} // Use URL as unique key
-              to={screen.url}
-              className={`text-lg w-full text-center font-bold px-4 py-2 transition-all ${
-                isActive
-                  ? "border-b-2 border-white text-white"
-                  : "text-gray-600 hover:border-b-2 "
-              }`}
-            >
-              {screen.title}
-            </Link>
-          );
-        })}
-      </div>
-    </div>
-  );
 };
 
 export default NavBar;
