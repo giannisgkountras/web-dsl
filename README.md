42ccd9e7a34e7404298da462f48730281a84f10d
giannisgkountras <gkountrasioannis@gmail.com>
2025-03-19 15:11:57 +0200

    Added live line chart component

--- README.md
+++ README.md
@@ -9,5 +9,17 @@
 ### Steps
 
 1. Clone the repository
-2. Run `docker-compose up`
+2. Run `docker compose up --build`
 3. Access the application at `http://localhost:8082`
+
+## Components
+
+### Notification Component
+
+-   Displays a notification message to the user
+-   Expects a message from the broker `{data: "Message to display"}`
+
+### Line Chart Component
+
+-   Displays a line chart with the data received from the broker
+-   Expects a message from the broker `{data: { x_axis: 1, y_axis: 10 }}`
--- web_dsl/generate.py
+++ web_dsl/generate.py
@@ -8,11 +8,13 @@ frontend_env = Environment(
     loader=FileSystemLoader(f"{os.path.dirname(__file__)}/templates/frontend"),
     trim_blocks=True,
     lstrip_blocks=True,
+    extensions=["jinja2.ext.loopcontrols"],
 )
 backend_env = Environment(
     loader=FileSystemLoader(f"{os.path.dirname(__file__)}/templates/backend"),
     trim_blocks=True,
     lstrip_blocks=True,
+    extensions=["jinja2.ext.loopcontrols"],
 )
 
 frontend_base_dir = os.path.join(os.path.dirname(__file__), "base", "frontend_base")
@@ -25,6 +27,9 @@ websocket_context_config_template = frontend_env.get_template(
     "websocket_context_config.jinja"
 )
 live_component_template = frontend_env.get_template("live_component.jinja")
+custom_line_chart_template = frontend_env.get_template(
+    "components/line_chart_template.jinja"
+)
 
 config_template = backend_env.get_template("config_template.jinja")
 dockerfile_template = backend_env.get_template("dockerfile_template.jinja")
@@ -54,6 +59,15 @@ def generate(model_path, gen_path):
     subprocess.run(
         ["cp", "-r", f"{backend_base_dir}/.", os.path.join(gen_path, "backend")]
     )
+
+    # Gather all live copmonents from the model
+    live_components = set()
+    for screen in model.screens:
+        for element in screen.elements:
+            collect_live_components(element, live_components)
+    live_components = list(live_components)
+    for element in live_components:
+        print(element.definition)
 
     # ========= Generate frontend files============
     # Prepare the output directories
@@ -94,15 +108,22 @@ def generate(model_path, gen_path):
         f.write(websocket_context_config_content)
     print(f"Generated: {websocket_context_config_output_file}")
 
+    # Generate component files
+    components_dir = os.path.join(gen_path, "frontend", "src", "components")
+
+    # CustomLineChart component
+    for component in live_components:
+        if component.definition.__class__.__name__ == "LineChart":
+            custom_line_chart_output_file = os.path.join(
+                components_dir, "CustomLineChart.jsx"
+            )
+            with open(custom_line_chart_output_file, "w", encoding="utf-8") as f:
+                f.write(custom_line_chart_template.render(element=component))
+            print(f"Generated: {custom_line_chart_output_file}")
+
     # ========= Generate backend files============
 
     # Gather all topics from the model
-    live_components = set()
-    for screen in model.screens:
-        for element in screen.elements:
-            collect_live_components(element, live_components)
-    live_components = list(live_components)
-
     all_topics = set()
     for component in live_components:
         all_topics.add(component.topic)
