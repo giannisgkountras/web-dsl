b3cef93a83c5a7d2a5100e34c44487a5ff0ff13c
giannisgkountras <gkountrasioannis@gmail.com>
2025-03-28 00:18:04 +0200

    Added error handling and image dimensions

--- web_dsl/base/frontend_base/src/utils/convertTypeValue.js
+++ web_dsl/base/frontend_base/src/utils/convertTypeValue.js
@@ -1,13 +1,25 @@
+import { toast } from "react-toastify";
+
 const convertTypeValue = (value, type) => {
-    switch (type) {
-        case "IntAttribute":
-            return parseInt(value);
-        case "FloatAttribute":
-            return parseFloat(value);
-        case "BoolAttribute":
-            return value === "true" || value === true;
-        default:
-            return value;
+    try {
+        if (value === undefined || value === null) {
+            throw new Error("Cannot convert undefined or null to object");
+        }
+
+        switch (type) {
+            case "IntAttribute":
+                return parseInt(value, 10);
+            case "FloatAttribute":
+                return parseFloat(value);
+            case "BoolAttribute":
+                return value === "true" || value === true;
+            default:
+                return value;
+        }
+    } catch (error) {
+        toast.error(error.message);
+        console.error("Conversion Error:", error);
+        return null; // Return a fallback value if conversion fails
     }
 };
 
