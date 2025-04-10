d572f54b83f1a5001fe68846e8929a0276cce01e
giannisgkountras <gkountrasioannis@gmail.com>
2025-04-10 12:29:07 +0300

    Mad jsonViewer accept work with rest

--- web_dsl/base/frontend_base/src/components/JsonViewer.jsx
+++ web_dsl/base/frontend_base/src/components/JsonViewer.jsx
@@ -1,16 +1,79 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import JsonView from "@uiw/react-json-view";
 import { useWebsocket } from "../hooks/useWebsocket";
 import { WebsocketContext } from "../context/WebsocketContext";
 import convertTypeValue from "../utils/convertTypeValue";
 import customTheme from "../utils/jsonviewtheme";
 import { toast } from "react-toastify";
+import { IoReload } from "react-icons/io5";
+import { proxyRestCall } from "../api/proxyRestCall";
 
-const JsonViewer = ({ topic, attributes = [] }) => {
-    const [jsonData, setJsonData] = React.useState({});
+const JsonViewer = ({ topic, attributes = [], sourceOfContent, restData }) => {
+    const [jsonData, setJsonData] = useState({});
     const ws = useContext(WebsocketContext);
 
-    useWebsocket(ws, topic, (msg) => {
+    const fetchValue = () => {
+        const { host, port, path, method, headers, params } = restData;
+
+        proxyRestCall({
+            host,
+            port,
+            path,
+            method,
+            headers,
+            params
+        })
+            .then((response) => {
+                try {
+                    if (attributes.length === 0) {
+                        // If no attributes are provided, set the entire message as JSON data
+                        try {
+                            setJsonData(response);
+                        } catch (error) {
+                            toast.error(
+                                "An error occurred while updating value: " +
+                                    error.message
+                            );
+                            console.error("Error updating status:", error);
+                        }
+                    } else {
+                        const newJsonData = {};
+                        attributes.forEach((attribute) => {
+                            try {
+                                newJsonData[attribute.name] = convertTypeValue(
+                                    response[attribute.name],
+                                    attribute.type
+                                );
+                            } catch (error) {
+                                toast.error(
+                                    "An error occurred while updating value: " +
+                                        error.message
+                                );
+                                console.error("Error updating status:", error);
+                            }
+                        });
+                        setJsonData(newJsonData);
+                    }
+                } catch (error) {
+                    toast.error(
+                        "An error occurred while converting value: " +
+                            error.message
+                    );
+                }
+            })
+            .catch((error) => {
+                toast.error("Error fetching initial value: " + error.message);
+                console.error("Error fetching initial value:", error);
+            });
+    };
+
+    useEffect(() => {
+        if (sourceOfContent === "rest") {
+            fetchValue();
+        }
+    }, []);
+
+    useWebsocket(sourceOfContent === "broker" ? ws : null, topic, (msg) => {
         if (attributes.length === 0) {
             // If no attributes are provided, set the entire message as JSON data
             try {
@@ -41,10 +104,21 @@ const JsonViewer = ({ topic, attributes = [] }) => {
         }
     });
     return (
-        <div className="flex justify-center flex-col items-center p-4 w-fit bg-[#111828] rounded-2xl">
-            <h1 className="mb-2 text-lg font-semibold text-gray-200">
-                Watching <span className="text-pink-400">{topic}</span>
-            </h1>
+        <div className="flex justify-center flex-col items-center p-4 w-fit bg-[#111828] rounded-2xl relative">
+            {"broker" === sourceOfContent && (
+                <h1 className="mb-2 text-lg font-semibold text-gray-200">
+                    Watching <span className="text-pink-400">{topic}</span>
+                </h1>
+            )}
+            {"rest" === sourceOfContent && (
+                <button
+                    className="absolute top-0 right-0 p-4 text-gray-100 hover:text-gray-500 hover:cursor-pointer z-10"
+                    onClick={fetchValue}
+                    title="Refresh Value"
+                >
+                    <IoReload size={24} />
+                </button>
+            )}
             <JsonView value={jsonData} style={customTheme} />
         </div>
     );
