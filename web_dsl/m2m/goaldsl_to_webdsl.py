96d6aca2a208849f87acd9cae4c2003cc09db2f0
giannisgkountras <gkountrasioannis@gmail.com>
2025-05-15 15:12:12 +0300

    Added boilerplate for goaldsl transformation

---
+++ web_dsl/m2m/goaldsl_to_webdsl.py
@@ -0,0 +1,15 @@
+def transform_goaldsl_to_webdsl(goaldsl_content):
+    """
+    Transforms GOAL DSL content to WebDSL content.
+
+    Args:
+        goaldsl_content (str): The GOAL DSL content to be transformed.
+
+    Returns:
+        str: The transformed WebDSL content.
+    """
+    # Placeholder for the transformation logic
+    # This should include parsing the GOAL DSL and converting it to WebDSL syntax
+    webdsl_content = "test"
+
+    return webdsl_content
