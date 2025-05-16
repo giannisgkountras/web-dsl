7ab72e700db83c81246e111b6831b3f220ad0436
giannisgkountras <gkountrasioannis@gmail.com>
2025-05-16 20:58:09 +0300

    Goaldsl to webdsl with entities, brokers, brokertopics, restapis, restendpoints supported

--- web_dsl/m2m/goaldsl_to_webdsl.py
+++ web_dsl/m2m/goaldsl_to_webdsl.py
@@ -62,10 +62,61 @@ def transform_goaldsl_to_webdsl(goaldsl_content):
         else:
             endpoint.webdsl_path = endpoint.path
 
+    # Extract all broker topics from entities and their corresponding broker
+    all_broker_topics = []
+    for entity in all_goal_entities:
+        source_type = entity.source.ref.__class__.__name__
+        if source_type == "RESTEndpoint":
+            # If the source is a REST endpoint, skip it
+            continue
+
+        broker_source = entity.source.ref.name
+        broker_topic = entity.uri
+
+        if broker_topic is not None:
+            # Check if the topic is already in the list with the same source
+            if not any(
+                topic
+                for topic in all_broker_topics
+                if topic.get("source", None) == broker_source
+                and topic.get("topic", None) == broker_topic
+            ):
+                all_broker_topics.append(
+                    {
+                        "name": f"{entity.name}_Topic",
+                        "source": broker_source,
+                        "topic": broker_topic,
+                    }
+                )
+
+    # Add the broker topic name to all the entities
+    for entity in all_goal_entities:
+        # Find the name of the broker topic via the uri and the source
+        source_type = entity.source.ref.__class__.__name__
+        if source_type == "RESTEndpoint":
+            # If the source is a REST endpoint, the source is the endpoint name
+            entity.webdsl_entity_source = entity.source.ref.name
+            continue
+
+        broker_source = entity.source.ref.name
+        broker_topic = entity.uri
+        if broker_topic is not None:
+            # Find the topic in the list
+            for topic in all_broker_topics:
+                if (
+                    topic.get("source", None) == broker_source
+                    and topic.get("topic", None) == broker_topic
+                ):
+                    entity.webdsl_entity_source = topic.get("name", None)
+                    break
+        else:
+            entity.webdsl_entity_source = None
+
     return template.render(
         entities=all_goal_entities,
         mqtt_brokers=all_goal_mqtt_brokers,
         amqp_brokers=all_goal_amqp_brokers,
         redis_brokers=all_goal_redis_brokers,
         rest_endpoints=all_goal_rest_endpoints,
+        all_broker_topics=all_broker_topics,
     )
